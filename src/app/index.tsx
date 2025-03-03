6c2925bcca9bd0eea8dbb49e03cf9803c888d4f6
SuryaS1729 <surya.curiousfusion@gmail.com>
2025-03-03 22:02:16 +0530

    rebase

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import CardComponent from '../components/CardComponent'
 
 const index = () => {
   return (
     <View >
      
-    <CardComponent/>
+
     
     </View>
   )
