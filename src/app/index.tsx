5ac3a5388f5501b344ca18aa57534e384d172a36
SuryaS1729 <surya.curiousfusion@gmail.com>
2025-03-04 19:35:37 +0530

    flatlist 102

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import ListData from '../components/ListData'
+import ArrayOfObjects from '../components/ArrayOfObjects'
 
 const index = () => {
   return (
     <View >
      
-    <ListData/>
+    <ArrayOfObjects/>
     
     </View>
   )
--- src/components/ArrayOfObjects.tsx
+++ src/components/ArrayOfObjects.tsx
@@ -1,10 +1,37 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
 
 const ArrayOfObjects = () => {
+    const bowlers = [
+        {id:1,
+            name:"bumrah"
+        },
+        {
+            id:2,
+            name:"shami"
+        },
+        {
+            id:3,
+            name:"chahal"
+        },
+        {
+            id:4,
+            name:"zampa"
+        },
+        {
+            id:5,
+            name:"hardik"
+        }
+    ]
   return (
     <View>
-      <Text>ArrayOfObjects</Text>
+        
+        <FlatList 
+        data = {bowlers} 
+        keyExtractor={(item) => item.id.toString()} //react expects a string so gotta do to strig 
+        renderItem={({item}) => <Text>{item.name}</Text>}
+        />
+        
     </View>
   )
 }
