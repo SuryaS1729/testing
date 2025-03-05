d4e1171eddafee34a40ce77eca2b0fae0a6f5cd1
SuryaS1729 <surya.curiousfusion@gmail.com>
2025-03-05 13:23:32 +0530

    chalecompe

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,12 +2,13 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import ListData from '../components/ListData'
 import ArrayOfObjects from '../components/ArrayOfObjects'
+import HugeList from '../components/HugeList'
 
 const index = () => {
   return (
     <View >
      
-    <ArrayOfObjects/>
+   <HugeList/>
     
     </View>
   )
