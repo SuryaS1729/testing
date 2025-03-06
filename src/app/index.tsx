3b10cb6bce82c584d0809a2854c2cb3e82143a76
SuryaS1729 <surya.curiousfusion@gmail.com>
2025-03-06 23:40:36 +0530

    adding functions to buttons with state

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,13 +2,13 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import WithoutState from '../components/1.Basic Counter/WithoutState'
 import WithState from '../components/1.Basic Counter/WithState'
+import UpdateArray from '../components/Update Arrays/UpdateArray'
 
 const index = () => {
   
   return (
     <View>
-        <WithoutState/>
-        <WithState/>
+       <UpdateArray/>
     </View>
   )
 }
