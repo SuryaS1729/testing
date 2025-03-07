d25479cff15687eff49e6b6eebf46da70a0879f3
SuryaS1729 <surya.curiousfusion@gmail.com>
2025-03-07 17:23:02 +0530

    sharing state

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,12 +4,13 @@ import WithoutState from '../components/1.Basic Counter/WithoutState'
 import WithState from '../components/1.Basic Counter/WithState'
 import UpdateArray from '../components/2.Update Arrays/UpdateArray'
 import UpdateObjects from '../components/3.Updating Objects/UpdateObjects'
+import SharingState from '../components/4.SharingState/SharingState'
 
 const index = () => {
   
   return (
     <View>
-<UpdateObjects/>
+<SharingState/>
     </View>
   )
 }
---
+++ src/components/4.SharingState/Component2.tsx
@@ -0,0 +1,18 @@
+import { View, Text, Button } from 'react-native'
+import React from 'react'
+
+interface ComponentPropTypes{
+    count:number;
+    onClickHandler:()=>void;
+}
+const Component2 = ({count, onClickHandler}:ComponentPropTypes) => {
+  return (
+    <View>
+      <Text>Component2</Text>
+      <Text>{count  }</Text>
+      <Button title='increment count' onPress={onClickHandler}></Button>
+    </View>
+  )
+}
+
+export default Component2
