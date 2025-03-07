e221595fafcd52b78818890ce89156a6e063127b
SuryaS1729 <surya.curiousfusion@gmail.com>
2025-03-07 18:28:07 +0530

    rebasing 2

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import WithoutState from '../components/1.Basic Counter/WithoutState'
-import WithState from '../components/1.Basic Counter/WithState'
-import UpdateArray from '../components/2.Update Arrays/UpdateArray'
-import UpdateObjects from '../components/3.Updating Objects/UpdateObjects'
-import SharingState from '../components/4.SharingState/SharingState'
 
 const index = () => {
   
   return (
     <View>
-<SharingState/>
+hi
     </View>
   )
 }
