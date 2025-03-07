2eb0c41b2bdc90d7ad16edaa72ca66515a6b2219
SuryaS1729 <surya.curiousfusion@gmail.com>
2025-03-07 17:14:22 +0530

    updating state

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,13 +2,14 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import WithoutState from '../components/1.Basic Counter/WithoutState'
 import WithState from '../components/1.Basic Counter/WithState'
-import UpdateArray from '../components/Update Arrays/UpdateArray'
+import UpdateArray from '../components/2.Update Arrays/UpdateArray'
+import UpdateObjects from '../components/3.Updating Objects/UpdateObjects'
 
 const index = () => {
   
   return (
     <View>
-       <UpdateArray/>
+<UpdateObjects/>
     </View>
   )
 }
